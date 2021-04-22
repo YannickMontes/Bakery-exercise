1c558ead2b1a5766a6d415e8bdad98184916e12b
YannickMontes <montes.yannick@gmail.com>
2021-04-22 22:05:35 +0200

    Corps app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-//Your main code here.
+
+// Nous avons besoin d'un objet bakery.
+
+// Ne modifiez pas la partie suivante
+bakery.enter("Patrick");
+bakery.exit("Patrick");
+bakery.enter("Anne");
+bakery.enter("Alicia");
+bakery.exit("Alicia");
+bakery.exit("Anne");
